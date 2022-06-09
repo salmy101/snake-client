6edd70f35420d3fdd3c22910cf2c84bb94c11b52
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-06-09 06:32:59 +0000

    finally done the snake game! added messages

--- client.js
+++ client.js
@@ -1,43 +1,30 @@
 const net = require('net');
 
 const connect = function () {
-const conn = net.createConnection({ //setting up a connection to the server
+  const conn = net.createConnection({ //setting up a connection to the server
   host:"165.227.47.243",
   port:"50541"
-});
-conn.setEncoding("utf8"); //interpret data as text
+  });
+  conn.setEncoding("utf8"); //interpret data as text
+
+  conn.on("connect", () => {
+    console.log("Connected!");
+    conn.write("Name: SAL");
+  });
+
+  conn.on("data", (data) => { // Return the message from server when you get kicked out for idling
+  console.log("Server says: ", data);
+  });  
+  // const setupInput = require("./input.js");
 
 
-conn.on("connect", () => {
-  conn.write("Conneted!");
-  conn.write("Name: SAL");
-  // setTimeout(() => {
-  //   conn.write('Move: up')
-  // })
-});
-
-const setupInput = require("./input.js");
-
-// const setupInput = function () {
-//   const stdin = process.stdin;
-//   stdin.setRawMode(true);
-//   stdin.setEncoding("utf8");
-//   stdin.resume();
-//   const handleUserInput = function () {
-//     stdin.on("data", handleUserInput);
-//     if (key === '\u0003') {
-//       process.exit();
-//     }
-//   };
-//   return stdin;
-// };
-
-
-return conn; 
+  return conn; 
 
 };
 
-console.log("Connecting...")
-connect(); 
+
+// console.log("Connecting ...");
+// connect();
+
 
 module.exports = connect;
--- input.js
+++ input.js
@@ -1,15 +1,34 @@
-const setupInput = function () {
+// const { connect } = require("http2");
+
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
+  stdin.on("data", handleUserInput);
   stdin.resume();
-  const handleUserInput = function () {
-    stdin.on("data", handleUserInput);
-    if (key === '\u0003') {
-      process.exit();
-    }
-  };
   return stdin;
 };
 
+const handleUserInput = function (data) {
+  if (data === '\u0003'){ // if they use this key then termminate
+    process.exit();
+  } else if ( data === "w"){ // if not move on, these are they only 4 keys that can be used
+    connection.write("Move: up");
+  }  else if (data === 'a') {
+    connection.write("Move: left");
+  } else if (data === 's') {
+    connection.write("Move: down");
+  } else if ( data === 'd') {
+    connection.write("Move: right")
+  }  
+  if(data === 'o') {
+    connection.write("Say: moooovveee!!");
+} else if (data === 'l') {
+  connection.write("Say: see ya");
+}
+};
 module.exports = setupInput;
+
