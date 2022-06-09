e03a7eaa6d2ae171d35c6caf34cdab6a77a0c2c6
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-06-09 02:07:35 +0000

    added a ctrl + c terminate ability

--- client.js
+++ client.js
@@ -5,12 +5,31 @@ const conn = net.createConnection({ //setting up a connection to the server
   host:"165.227.47.243",
   port:"50541"
 });
+conn.setEncoding("utf8"); //interpret data as text
+
 
 conn.on("connect", () => {
+  conn.write("Conneted!");
   conn.write("Name: SAL");
+  // setTimeout(() => {
+  //   conn.write('Move: up')
+  // })
 });
 
-conn.setEncoding("utf8"); //interpret data as text
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  const handleUserInput = function () {
+    stdin.on("data", handleUserInput);
+    if (key === '\u0003') {
+      process.exit();
+    }
+  };
+  return stdin;
+};
+
 
 return conn; 
 
